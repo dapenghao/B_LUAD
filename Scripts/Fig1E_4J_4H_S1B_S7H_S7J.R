81f9e07a137eea29df9d4d73230e3cc5f1f8b06e
Dapeng Hao <34309900+dapenghao@users.noreply.github.com>
2022-07-22 17:21:13 +0800

    Add files via upload

---
+++ Scripts/Fig1E_4J_4H_S1B_S7H_S7J.R
@@ -0,0 +1,124 @@
+library(ggplot2);library(data.table);library(Seurat);library(dplyr);library(tidyr);library(ggpubr)
+
+Bcell <- readRDS("Bcell.rds")
+load("celltype_2.colors.rda")
+#----------------------------------------------------------------------------------------------------------
+#### Fig.1E, Fig.S1B ####
+#----------------------------------------------------------------------------------------------------------
+load("10Xclone_mutation.rda")
+# Fig.1E
+{
+  p1 <- DimPlot(Bcell, reduction = "umap",label =F,group.by = "celltype_2",cols=celltype_2.colors,pt.size=1)
+  p1 <- AugmentPlot(p1)
+  
+  my.colors <- c(smoker="#cb181d",`never smoker`="#fee0d2")
+  p2 <- DimPlot(Bcell, reduction = "umap",label =F,group.by = "Smoking2",cols=my.colors,pt.size=1,shuffle=T)
+  p2 <- AugmentPlot(p2)
+  
+  my.colors <- c(tumor="#8c6bb1",adjacent="#4292c6",intermediate="#9ecae1",distant="#deebf7")
+  p3 <- DimPlot(Bcell, reduction = "umap",label =F,group.by = "Field",cols=my.colors,pt.size=1,shuffle=T)
+  p3 <- AugmentPlot(p3)
+  
+  
+  load("./10Xclone_mutation.rda")
+  iOrd <- intersect(patient_obs$cell_id,colnames(Bcell))
+  Bcell.BCR <- subset(Bcell,cells=iOrd)
+  Bcell.BCR$Isotype <- patient_obs[colnames(Bcell.BCR),"c_call"]
+  Bcell.BCR$MF_group <- patient_obs[colnames(Bcell.BCR),"MF_group"]
+  Bcell.BCR$Field <- ifelse(Bcell.BCR$Field=="tumor","tumor","normal")
+  Bcell.BCR$Mut <- paste0(Bcell.BCR$Field,"-",Bcell.BCR$Mut)
+  Bcell.noBCR <- subset(Bcell,cells=setdiff(colnames(Bcell),patient_obs$cell_id))
+  Bcell.noBCR$Isotype="noBCR"
+  
+  isotype.colors <- c(IGHA1="#fec44f",IGHA2="#fe9929",IGHG1="#c6dbef",IGHG2="#6baed6",IGHG3="#2171b5",IGHG4="#08306b",
+                      IGHD="#807dba",IGHE="#D51F26",noBCR="#d9d9d9",IGHM="#fa9fb5")
+  p4 <- DimPlot(Bcell.BCR, reduction = "umap",label =F,group.by = "Isotype",cols=isotype.colors,pt.size=1.5)
+  p4 <- AugmentPlot(p4)
+}
+
+# Fig.S1B
+{
+  p1 <- FeaturePlot(Bcell,features = c("FCER2","MZB1","JCHAIN","AIM2"))
+  p1 <- AugmentPlot(p1)
+}
+#----------------------------------------------------------------------------------------------------------
+#### Fig.4J ####
+#----------------------------------------------------------------------------------------------------------
+load("10Xclone_mutation.rda")
+patient_obs$MF_group[patient_obs$MF_group %in% c("Medium","High")] <- "High"
+ggplot(patient_obs, aes(x = MF_group, y = log10(clone_size),fill = MF_group)) + 
+  geom_boxplot(outlier.shape = NA)+
+  scale_fill_manual(values=my.colors)+
+  theme_classic()+ylab("Clone size")+ylim(c(0,3.5))
+#----------------------------------------------------------------------------------------------------------
+#### Fig.4H, Fig.S7H ####
+#----------------------------------------------------------------------------------------------------------
+load("10Xclone_mutation.rda")
+#Fig.4H:
+{
+  iOrd <- intersect(patient_obs$cell_id,colnames(Bcell))
+  Bcell.mut <- subset(Bcell,cells=iOrd)
+  Bcell.mut$MF <- patient_obs[colnames(Bcell.mut),"mu_freq_seq_r"]
+  Bcell.mut$MF_group <- Bcell.mut$MF
+  Bcell.mut$MF_group <- case_when(
+    Bcell.mut$MF == 0 ~ "Germline",
+    Bcell.mut$MF <=0.03 ~ "Low",
+    #Bcell.mut$MF <=0.06 ~ "Medium",
+    Bcell.mut$MF >0.03  ~ "High")
+  
+  my.colors <- c(Germline="#74add1",Low="#ffffbf",Medium="#fdae61",High="#d73027")
+  p1 <- DimPlot(Bcell.mut, reduction = "umap",label =F,group.by = "MF_group",cols=my.colors,pt.size=1)
+  p1 <- AugmentPlot(p1)
+}
+
+#Fig.S7H:
+{
+  iOrd <- intersect(patient_obs$cell_id,colnames(Bcell))
+  Bcell.mut <- subset(Bcell,cells=iOrd)
+  Bcell.mut$MF <- patient_obs[colnames(Bcell.mut),"mu_freq_seq_r"]
+  Bcell.mut$MF_group <- Bcell.mut$MF
+  Bcell.mut$MF_group <- case_when(
+    Bcell.mut$MF == 0 ~ "Germline",
+    Bcell.mut$MF <=0.03 ~ "Low",
+    Bcell.mut$MF >0.03  ~ "High")
+  
+  my.colors <- c(Germline="#74add1",Low="#ffffbf",High="#d73027")
+  p1 <- DimPlot(Bcell.mut, reduction = "umap",label =F,group.by = "MF_group",cols=my.colors,pt.size=0.5,split.by = "Mut")
+  p1 <- AugmentPlot(p1)
+  pdf("png_UMAPs of cells by mutation groups splitted.pdf",width = 18,height=6)
+  print(p1)
+  dev.off()
+}
+
+#----------------------------------------------------------------------------------------------------------
+#### Fig.S7J ####
+#----------------------------------------------------------------------------------------------------------
+library(scales)
+load("10Xclone_mutation.rda")
+my.colors <- c(Germline="#74add1",Low="#ffffbf",High="#d73027")
+patient_obs <- patient_obs[patient_obs$Field=="tumor",]
+patient_obs$MF_group[patient_obs$MF_group %in% c("Medium","High")] <- "High"
+x <- table(patient_obs$MutGenes)/nrow(patient_obs)
+expected <- as.numeric(x);names(expected) <- names(x)
+table(patient_obs$MF_group)
+df <- NULL
+MF_group <- unique(paste0(patient_obs$MF_group))
+for (i in MF_group) {
+  x <- patient_obs[patient_obs$MF_group==i,]
+  x <- table(x$MutGenes)/nrow(x)/expected
+  x <- data.frame(MF_group=i,MutGenes=names(x),Roe=as.numeric(x))
+  df <- rbind(df,x)
+}
+
+labs <- as.character(round(df$Roe,2))
+df2 <- df;df2$Roe[df2$Roe>=2.5]=2.5#change the max value to 2
+df2$MF_group <- factor(df2$MF_group,levels=names(my.colors))
+p1 <- ggplot(data =  df2, aes(x = MutGenes, y = MF_group)) + 
+  geom_tile(aes(fill = Roe)) +
+  scale_fill_gradientn(colours=c("#41ae76","#99d8c9","#f0f0f0","#feb24c","#fc4e2a"),
+                       values=rescale(c(0.5,0.75,1,1.75,2.5)),
+                       guide="colorbar")
+p1 +  geom_text(aes(label=labs), size=4) + theme_classic()
+
+
+
---
+++ Scripts/Fig1F_S1A_S10B_S10D.R
@@ -0,0 +1,67 @@
+setwd("/Users/dhao/Desktop/projects/Bcell_lung")
+library(ggplot2);library(data.table);library(Seurat);
+library(dplyr);library(tidyr);library(ggpubr);library(RColorBrewer)
+
+Bcell <- readRDS("Bcell.rds")
+load("celltype_2.colors.rda")
+Bcell$celltype_2 <- factor(Bcell$celltype_2,levels = c("NaiveB","MemB-1","MemB-2","Plasmablast","Plasma-IgD",
+                                                       "Plasma-IgM","Plasma-IgA1","Plasma-IgG1","Plasma-IgG3",
+                                                       "Plasma-IgA2","Plasma-IgG2","Stressed plasma"))
+#----------------------------------------------------------------------------------------------------------
+#### Fig.1F ####
+#----------------------------------------------------------------------------------------------------------
+x=c("MS4A1","CD19","BANK1","SELL","IL4R","FCER2","TCL1A","BACH2",#Pan-B and Naive B cells
+    "CD27","CD38","JCHAIN","MZB1","SDC1","TNFRSF17",#Plasma and memory B (CD27)
+    "IGHM", "IGHD","IGHG1", "IGHG2", "IGHG3", "IGHG4", "IGHA1", "IGHA2",#plasma isotype
+    "MKI67","TUBB","STMN1","TYMS",# plasmablast
+    "BAG3","HSPA6","HSPB1"# Stressed Plasma
+)
+DotPlot(Bcell, features = x,assay = "RNA",scale = T,group.by = "celltype_2") + 
+  scale_colour_gradientn(colors=brewer.pal(10, "BuPu")) + theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_discrete(breaks=x,labels=x)+ ylab("thres>0")
+#----------------------------------------------------------------------------------------------------------
+#### Fig.S1A ####
+#----------------------------------------------------------------------------------------------------------
+MHC2 <- c("CD74","HLA-DMA","HLA-DMB","HLA-DOA","HLA-DOB","HLA-DPA1",
+          "HLA-DPB1","HLA-DQA1","HLA-DQA2","HLA-DQB1","HLA-DQB2",
+          "HLA-DRA","HLA-DRB1","HLA-DRB3","HLA-DRB4","HLA-DRB5")
+MHC2 <- MHC2[MHC2 %in% rownames(Bcell)]
+DotPlot(object = Bcell, features = MHC2,scale = T,group.by = "celltype_2") + 
+  scale_colour_gradientn(colors=brewer.pal(10, "BuPu")) + theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_discrete(breaks=MHC2,labels=MHC2)#MHC2
+#----------------------------------------------------------------------------------------------------------
+#### Fig.10B ####
+#----------------------------------------------------------------------------------------------------------
+load("./Revision/Github/Input_data/MarkerExp.rda")
+load("./Revision/Github/Input_data/cellType_All.rda")
+df <- unique(Bcell@meta.data[,c("orig.ident","Field")]);rownames(df) <- df$orig.ident
+cellType_All$Field <- df[cellType_All$orig.ident,"Field"]
+obj <- CreateSeuratObject(t(MarkerExp),assay = "RNA",meta.data = cellType_All)
+obj@assays$RNA@data <- as.matrix(t(MarkerExp))
+obj <- subset(obj,subset=cellType %in% c("CD4T","CD8T","Fibroblast","Myeloid"))
+obj$cellType <- paste0(obj$Field,"-",obj$cellType)
+p1 <- DotPlot(object = obj, features = colnames(MarkerExp),assay = "RNA",scale = T,group.by = "cellType") + 
+  scale_colour_gradientn(colors=brewer.pal(9, "BuPu"))  + theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_discrete(breaks=colnames(MarkerExp),labels=colnames(MarkerExp))
+
+#----------------------------------------------------------------------------------------------------------
+#### Fig.10D ####
+#----------------------------------------------------------------------------------------------------------
+Bcell$celltype_1[Bcell$celltype_2 %in% c("MemB-1","MemB-2")] <- "MemB"
+Bcell$celltype_1[Bcell$celltype_2 %in% c("NaiveB")] <- "NaiveB"
+Bcell$celltype_1[grep("IgA",Bcell$celltype_2)] <- "IgA-Plasma"
+Bcell$celltype_1[grep("IgG",Bcell$celltype_2)] <- "IgG-Plasma"
+Bcell2 <- subset(Bcell,subset=celltype_1 %in% c("NaiveB","MemB","IgA-Plasma","IgG-Plasma"))
+Bcell2$celltype_1 <- factor(Bcell2$celltype_1,levels = c("NaiveB","MemB","IgA-Plasma","IgG-Plasma"))
+sel.genes <- c("CCR7","P2RY8","GPR183","TGFB1","CD24","NR4A1",
+               "NR4A2","LTB","BTLA","CCR10","VSIR","TNFRSF18","LGALS3")
+DotPlot(object = Bcell2, features = sel.genes,assay = "RNA",scale = T,group.by = "celltype_1") + 
+  scale_colour_gradientn(colors=brewer.pal(9, "BuPu"))  + theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_discrete(breaks=sel.genes,labels=sel.genes)
+
+
+
